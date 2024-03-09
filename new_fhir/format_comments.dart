5bfd8fd6b059ae3496a54781495b2807251f4dd1
Dokotela <grey.faulkenberry@mayjuun.com>
2024-03-08 23:12:25 -0500

    trying for comments

--- new_fhir/format_comments.dart
+++ new_fhir/format_comments.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 Future<void> main() async {
-  final Directory dir = Directory('.');
+  final Directory dir = Directory('../r5');
   final List<String> fileList = await dir
       .list(recursive: true)
       .map((FileSystemEntity event) => event.path)
